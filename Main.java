c46b07cc9332610ed7554e5f5bf1ffce05b0e280
Inammhd <inamhirai224@gmail.com>
2026-07-01 15:06:46 +0900

    HoT: initial revision

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+class Main{
+    public static void main(String args[]){
+        int h = 0, t = 0;
+        Random random = new Random();
+
+        System.out.println("Tossing a coin...");
+        
+        for(int i = 0;i < 3;i++){
+            if(random.nextInt(2) == 1){
+                System.out.println("Round " + (i + 1) +": Heads");
+                h++;
+            }
+            else{
+                System.out.println("Round " + (i + 1) +": Tails");
+                t++;    
+            }
+        }
+        
+        System.out.println("Heads: "+ h +", Tails: "+ t);
+
+    }
+}
